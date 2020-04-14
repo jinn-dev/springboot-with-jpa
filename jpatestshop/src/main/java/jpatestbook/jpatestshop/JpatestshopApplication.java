3438c291fe1a1fb1958ea22d1dca41b86c6f2ea1
jinn-dev <gsbyyy@naver.com>
2020-04-14 17:51:24 +0900

    initial commit

---
+++ jpatestshop/src/main/java/jpatestbook/jpatestshop/JpatestshopApplication.java
@@ -0,0 +1,13 @@
+package jpatestbook.jpatestshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpatestshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpatestshopApplication.class, args);
+	}
+
+}
---
+++ jpatestshop/src/test/java/jpatestbook/jpatestshop/JpatestshopApplicationTests.java
@@ -0,0 +1,13 @@
+package jpatestbook.jpatestshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpatestshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
